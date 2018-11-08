bf69dff403d53665c78795ee19542e45303b0223
Tigran <tigran.m6.y@tumo.org>
2018-11-08 17:08:03 +0400

    commited

---
+++ testList/ListTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace testList
+{
+    public class ListTest<T> : IEnumerable
+    {
+        public T value;
+        public ListTest<T> next;
+        private ListTest<T> _current = null;
+        public ListTest(T value)
+        {
+            this.value = value;
+        }
+        public void Add(T value) 
+        {
+            if (_current == null) 
+            {
+                next = new ListTest<T>(value);
+                _current = next;
+            }
+            else 
+            {
+                _current.next = new ListTest<T>(value);
+                _current = _current.next;
+            }
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+		public IEnumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+        private class Enumerator : IEnumerator
+        {
+            public Enumerator(ListTest<T> list)
+            {
+                this.list = list;
+            }
+            ListTest<T> list;
+            public object Current { get; private set; }
+
+            public bool MoveNext()
+            {
+                if (list == null) return false;
+
+                Current = list.value;
+                list = list.next;
+                return true;
+            }
+
+            public void Reset()
+            {
+                list = null;
+            }
+        }
+	}
+}
---
+++ testList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace testList
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ListTest<int> list = new ListTest<int>(10);
+            for (int i = 20; i <= 150; i+=10)
+            {
+                list.Add(i);
+            }
+            ListTest<double> list2 = new ListTest<double>(10);
+            for (double i = 0.1; i <= 10; i += 0.15)
+            {
+                list2.Add(i);
+            }
+            PrintList(list);
+            PrintList(list2);
+        }
+        static void PrintList<T>(ListTest<T> list)
+        {
+            foreach(T item in list) {Console.WriteLine(item);}
+        }
+    }
+}
